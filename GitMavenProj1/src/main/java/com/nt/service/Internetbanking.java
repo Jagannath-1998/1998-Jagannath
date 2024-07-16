1ebbaa0e736a06de70df7e3e4beae55623fc7a84
Jagannath <Jagannath@Jagannath_Sahu>
2024-07-16 21:38:23 +0530

    commit changes

--- GitMavenProj1/src/main/java/com/nt/service/Internetbanking.java
+++ GitMavenProj1/src/main/java/com/nt/service/Internetbanking.java
@@ -2,9 +2,16 @@ package com.nt.service;
 
 public class Internetbanking {
 public String internet() {
+	
+	
 	return  "payment done by internetbanking";
 }
 public void run() {
 	System.out.println("hii");
 }
+public int run1() {
+	
+return 1;
 }
+}
+
